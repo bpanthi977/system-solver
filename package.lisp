7d3f500fe40ac55a834c6c37363eaf7c138f5ad3
Bibek Panthi <bpanthi977@gmail.com>
2019-12-14 06:27:30 +0545

    moved hydraulics to new project. exported symbols from package

--- package.lisp
+++ package.lisp
@@ -1,4 +1,25 @@
 ;;;; package.lisp
 
 (defpackage #:system-solver
-  (:use #:cl))
+  (:use #:cl)
+  (:export
+   ;; base
+   #:parameter
+   #:relation
+   #:implicit-relation
+   #:solve-relation
+   #:eval-relation
+   #:parameters
+   #:value
+   #:relations
+   ;; interface
+   #:define-relation
+   #:define-component
+   #:relation-implicit-managed
+   #:with-parameters
+   #:satisfying-relations
+   #:component
+   #:paramter-slots
+   ;; system-solver
+   #:solve-for))
+   
